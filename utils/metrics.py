08582be7075dbb0d1fa4e34da63bf591abb0f6eb
CassioLeodegario <cassio.leodegario@gmail.com>
2025-07-12 11:38:54 -0300

    fix metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,16 +3,16 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import tensorflow as tf
 from tensorflow.keras import backend as K
-import config  # Certifique-se que config.N_CLASSES está definido
+import config  
 
 def dice_coef(y_true, y_pred, smooth=1e-7):
     """
     Dice multiclasse com one-hot automático em y_true.
     y_true: (batch, H, W)
-    y_pred: (batch, H, W, N_CLASSES)
+    y_pred: (batch, H, W, NUM_CLASSES)
     """
     # One-hot em y_true
-    y_true = tf.cast(tf.one_hot(tf.cast(y_true, tf.int32), depth=config.N_CLASSES), tf.float32)
+    y_true = tf.cast(tf.one_hot(tf.cast(y_true, tf.int32), depth=config.NUM_CLASSES), tf.float32)
 
     # y_pred já é softmax, só garantir tipo
     y_pred = tf.cast(y_pred, tf.float32)
@@ -28,7 +28,7 @@ def iou(y_true, y_pred, smooth=1e-7):
     """
     IOU multiclasse com one-hot automático em y_true.
     """
-    y_true = tf.cast(tf.one_hot(tf.cast(y_true, tf.int32), depth=config.N_CLASSES), tf.float32)
+    y_true = tf.cast(tf.one_hot(tf.cast(y_true, tf.int32), depth=config.NUM_CLASSES), tf.float32)
     y_pred = tf.cast(y_pred, tf.float32)
 
     intersection = K.sum(y_true * y_pred, axis=[1, 2])
